e20dde15cca9dbab30b33e4f107900778d7be339
A40nE <nischalkarki1661@gmail.com>
2022-02-01 18:53:23 +0545

    done project part

--- components/Cards/ProjectsHomeCard.jsx
+++ components/Cards/ProjectsHomeCard.jsx
@@ -19,9 +19,10 @@ export class ProjectsHomeCard extends Component {
         },
       ],
       ourProjects: {
-        head: [{ title: "our projects", progress: 30 }],
-        body: [
+        head: [
           {
+            title: "our projects",
+            progress: 30,
             description:
               " publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. In publishing and graphic design, Lorem ipsum is a placeholder text",
           },
@@ -31,14 +32,11 @@ export class ProjectsHomeCard extends Component {
   }
   render() {
     return (
-      <div className="h-96   flex flex-row  bg-gray-100  mt-10  ">
-        <div className="  flex flex-row    justify-center items-center">
+      <div className="h-96   flex flex-row  bg-gray-100  mt-10   ">
+        <div className=" flex-1 flex flex-row  w-full  justify-center items-center">
           {this.state.data.map((val, i) => {
             return (
-              <div
-                key={i}
-                className="h-fit  w-fit  flex flex-col items-start 	"
-              >
+              <div key={i} className="h-fit    m-5 flex flex-col items-start 	">
                 <div
                   src={val.image}
                   alt="images"
@@ -51,14 +49,12 @@ export class ProjectsHomeCard extends Component {
                     height: "100px",
                     width: "200px",
                   }}
-
-                  // bg-gradient-to-t bg-cover bg-no-repeat bg-center
                 />
-                <div className="h-fit mt-8 text-lg	capitalize  ">
+                <div className="h-fit mt-10 text-lg	capitalize  ">
                   {" "}
                   {val.title}
                 </div>
-                <div className="h-fit mt-1 w-fit text-left   text-sm">
+                <div className="h-fit mt-1 w-full text-left   text-sm">
                   {" "}
                   {val.description}
                 </div>
@@ -66,8 +62,8 @@ export class ProjectsHomeCard extends Component {
             );
           })}
         </div>
-        <div className=" flex flex-col  w-fit  justify-center items-center">
-          <div className="h-fit  w-fit items-start ">
+        <div className=" flex-1 h-96 flex justify-center  items-center w -full    ">
+          <div className="h-fit w-fit flex  flex-col   items-start m-5 ">
             {this.state.ourProjects.head.map((val, i) => {
               return (
                 <div
@@ -78,21 +74,18 @@ export class ProjectsHomeCard extends Component {
                     {val.title}
                   </div>
                   {val.progress}
+                  <div className="h-40 mt-1 w-full text-left overflow-scroll text-sm">
+                    {val.description}
+                  </div>
                 </div>
               );
             })}
-            <div className="w-fit ">
-              {this.state.ourProjects.body.map((val, i) => {
-                return (
-                  <div className="  w-fit text-left  text-sm">
-                    {" "}
-                    {val.description}
-                  </div>
-                );
-              })}
-            </div>
-            <button className="bg-white   text-blue-800  capitalize  mt-5 btn-outline-primary	
-            flex items-start  rounded-3xl ">view projects</button>
+            <button
+              className="bg-white   text-blue-800  capitalize  mt-3 btn-outline-primary	
+            flex items-start  rounded-3xl "
+            >
+              view projects
+            </button>
           </div>
         </div>
       </div>
