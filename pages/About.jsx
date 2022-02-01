a6ccb65a41a5555f7dd02f65a0f77727ee21bf7b
kisan2 <kisanmaat2@gmail.com>
2022-02-01 14:09:33 +0545

    introduction about us

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
-import React from "react";
 import PageTemplate from "../components/Templates/PageTemplate";
+import React from "react";
 
 function About() {
   return (
@@ -14,7 +14,39 @@ function About() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <PageTemplate>I'm About</PageTemplate>
+      <PageTemplate>
+        {/* About us introduction */}
+        <div className="h-fit flex w-full mt-5 ">
+          <div className="flex-1 h-96">
+            <div className="bg-grayD h-5/6 m-10 flex justify-center items-center">
+               <p className="text-white ">Video modal</p>
+            </div>
+          </div>
+          <div className="flex-1 flex flex-col  mt-10">
+            <p className="text-base text-gray-400 m-2 mt-0 ">About us</p>
+            <p className="text-xl text-primary font-bold m-2 tracking-wide "> Who we are ?</p>
+            
+            <p className="text-sm w-5/6  m-2 -tracking-tight">
+              In publishing and graphic design, Lorem ipsum is a placeholder
+              text commonly used to demonstrate the visual form of a document or
+              a typeface without relying on meaningful content. In publishing
+              and graphic design, Lorem ipsum is a placeholder text commonly
+              used to demonstrate the visual form of a document or a typeface
+              without relying on meaningful content. In publishing and graphic
+              design,
+            </p>
+            <Link href={"##"} > 
+            <div className="bg-transparent w-fit p-0.5 px-3 border-2 m-2
+             border-indigo-600 rounded-3xl text-primary text-sm
+             mt-5
+              "> Contact us
+            </div>
+            </Link>
+          </div>
+        </div>
+
+        {/*  */}
+      </PageTemplate>
     </div>
   );
 }
