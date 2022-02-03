63fb9c1a2ec86187258feba6b61f63363cdb62b3
A40nE <nischalkarki1661@gmail.com>
2022-02-03 14:39:57 +0545

    done fully

--- components/Ourclients/OurClients.jsx
+++ components/Ourclients/OurClients.jsx
@@ -1,53 +1,62 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+// import React from "react";
 
-export class OurClients extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      head: [{ title: "We have worked with...", data: "our clients" }],
-      body: [
-        { name: "gofundme", icon: "", width: "20%" },
-        { name: "TATA", icon: "", width: "10%" },
-        { name: "eventbrite", width: "20%" },
-        { name: "box", width: "10%" },
-        { name: "Nordea", width: "20%" },
-        {
-          name: "SurveyMonkey",
-          icon: "",
-          width: "20%",
-          fontWeight: "font-normal",
-        },
-      ],
-    };
-  }
-  render() {
-    return (
-      <div
-        className="h-96  w-full  flex   flex-col justify-center  items-center 
-      "
-      >
-        {this.state.head.map((val, i) => {
-          return (
-            <div
-              key={i}
-              className=" w-3/5 h-fit  flex flex-col justify-end items-center     "
-            >
-              <div className="text-3xl text-blue-600 font-medium ">
-                {" "}
-                {val.title}
-              </div>
-              <div className="text-gray-500 mt-3 capitalize text-xl">{val.data}</div>
+function OurClients() {
+  const [head, SetHead] = useState([
+    { title: "We have worked with...", data: "our clients" },
+  ]);
+  const [body, setBody] = useState([
+    { name: "gofundme", icon: "", width: "20%" },
+    { name: "TATA", icon: "", width: "10%" },
+    { name: "eventbrite", width: "20%" },
+    { name: "box", width: "10%" },
+    { name: "Nordea", width: "20%" },
+    {
+      name: "SurveyMonkey",
+      icon: "",
+      width: "20%",
+      fontWeight: "font-normal",
+    },
+  ]);
+
+  return (
+    <div className="flex justify-center h-96  flex-col items-center  ">
+      {head.map((val, i) => {
+        return (
+          <div
+            key={i}
+            className=" w-3/5 h-fit  flex flex-col justify-end items-center     "
+          >
+            <div className="text-3xl text-blue-600 font-medium ">
+              {" "}
+              {val.title}
             </div>
-          );
-        })}
-        <div className="flex  flex-row  flex-wrap h-48 m-5  justify-evenly items-center">
-          {this.state.body.map((val, i) => {
+            <div className="text-gray-500 mt-3 capitalize text-xl">
+              {val.data}
+            </div>
+          </div>
+        );
+      })}
+      <div className="h-fit lg:w-10/12 md:w-full  flex    flex-wrap flex-col flex-wrap  items-center ">
+        <div className="flex  flex-row  flex-wrap h-48 lg:w-6/12 md:w-10/12      justify-center items-end">
+          {body.map((val, i) => {
             return (
               <div
                 key={i}
                 width={val.width}
-                className={`text-4xl ml-5 text-gray-600 w-fit 
-                p-1 ${i === 5 ? "font-medium" : "font-bold"}`}
+                className={`
+                 ml-8 text-gray-600 w-fit 
+                 font-non italic
+                p-1 ${i === 5 ? "font-medium" : "font-bold"}
+                ${
+                  i === 1
+                    ? "text-xl"
+                    : i === 5 
+                    ? "text-2xl"
+                    : i === 0 || i==3
+                    ? "text-3xl"
+                    : "text-4xl"
+                }`}
               >
                 {val.name}
               </div>
@@ -55,8 +64,8 @@ export class OurClients extends Component {
           })}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default OurClients;
