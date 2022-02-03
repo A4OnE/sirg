a842416c114bf011291ab3fd1e605cd0f5cdf5d1
ashish44khanal <ashish24khanal@gmail.com>
2022-02-03 16:56:03 +0545

    latest changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,52 +19,56 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <PageTemplate>
-        <HomeSlider />
+        <div className="container mx-auto px-4 lg:px-8">
+          <HomeSlider />
 
-        {/* about us section starts  */}
-        <div
-          className="text-center my-24 py-10 "
-          style={{ backgroundImage: `url(${background})` }}
-        >
-          <div className="">
-            <h1>About Us</h1>
-            <hr className="w-96 mx-auto border-2" />
-            <p className="my-10 leading-8 lg:p-10">
-              In publishing and graphic design, Lorem ipsum is a placeholder
-              text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. In publishing
-              and graphic design, Lorem ipsum is a placeholder text commonly
-              used to demonstrate the visual form of a document or a typeface
-              without relying on meaningful content. In publishing and graphic
-              design, Lorem ipsum is a placeholder text commonly used to
-              demonstrate the visual form of a document or a typeface without
-              relying on meaningful content.
-            </p>
+          {/* about us section starts  */}
+          <div
+            className="text-center my-24 py-10 "
+            style={{ backgroundImage: `url(${background})` }}
+          >
+            <div className="">
+              <h1>About Us</h1>
+              <hr className="w-96 mx-auto border-2" />
+              <p className="my-10 leading-8 lg:p-10">
+                In publishing and graphic design, Lorem ipsum is a placeholder
+                text commonly used to demonstrate the visual form of a document
+                or a typeface without relying on meaningful content. In
+                publishing and graphic design, Lorem ipsum is a placeholder text
+                commonly used to demonstrate the visual form of a document or a
+                typeface without relying on meaningful content. In publishing
+                and graphic design, Lorem ipsum is a placeholder text commonly
+                used to demonstrate the visual form of a document or a typeface
+                without relying on meaningful content.
+              </p>
+            </div>
+            <div className="my-10">
+              <Link href={"/About"}>
+                <button className="btn-primary rounded-full">
+                  More About Us
+                </button>
+              </Link>
+            </div>
           </div>
-          <div className="my-10">
-            <Link href={"/About"}>
-              <button className="btn-primary rounded-full">
-                More About Us
-              </button>
-            </Link>
-            <ProjectsHomeCard/>
+
+          <ProjectsHomeCard />
+
+          {/* about us section ends  */}
+          {/* Service section starts here */}
+          <div className="text-center py-10">
+            <h1 className="text-normal">What do we Do ?</h1>
+            <p className="">Services we offer to our custormers</p>
+            <div className="my-8">
+              <Link href={"/Services"}>
+                <button className="btn-outline-primary rounded-full">
+                  View All
+                </button>
+              </Link>
+            </div>
           </div>
+          <HomePageSeviceSection />
+          {/* service section ends here  */}
         </div>
-        {/* about us section ends  */}
-        {/* Service section starts here */}
-        <div className="text-center py-10">
-          <h1 className="text-normal">What do we Do ?</h1>
-          <p className="">Services we offer to our custormers</p>
-          <div className="my-8">
-            <Link href={"/Services"}>
-              <button className="btn-outline-primary rounded-full">
-                View All
-              </button>
-            </Link>
-          </div>
-        </div>
-        <HomePageSeviceSection />
-        {/* service section ends here  */}
       </PageTemplate>
     </div>
   );
