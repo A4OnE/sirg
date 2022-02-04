c5cf72b353c2e985c52aea6a4edf74527d982094
kisan2 <kisanmahat2@gmail.com>
2022-02-04 11:42:11 +0545

    fixed

--- components/events_card/Event_Card.jsx
+++ components/events_card/Event_Card.jsx
@@ -3,17 +3,6 @@ import React from "react";
 
 function Event_Card({ index }) {
   return (
-<<<<<<< HEAD
-    <div className={`border border-black rounded-r-lg h-80
-    w-5/12 mt-20 ml-5 bg-stone-500  ${index%2===0?'mr-28':null}
-    flex flex-row  shadow-base shadow-zinc-500 md:w-5/12 h-64 ml-12 mr-0
-    
-    `}>
-      <div className="h-5/5 w-1/5 bg-stone-500"></div>
-
-      <div className="h-5/5 w-4/5 rounded-r-lg pl-5  bg-white ">
-        <p className="text-2xl font-bold mt-10 ml-1 text-zinc-600 md:text-xl md:mt-5 ">
-=======
     <div className="flex flex-col lg:flex-row border transition cursor-pointer hover:shadow-lg">
       {/* left img section  */}
       <div className=" h-44 w-full lg:h-full lg:w-1/2 bg-stone-500"></div>
@@ -21,15 +10,14 @@ function Event_Card({ index }) {
       {/* right content section  */}
       <div className="bg-white p-4 lg:p-8">
         <p className="text-2xl font-bold text-zinc-600">
->>>>>>> c86362ade8c8bf1273c5a19dab2d8766949d4383
           This is the first event title which goes here
         </p>
-        <div className="mt-5 mb-8  flex flex-row text-base items-center md:mt-2 md:mb-2 ">
+        <div className="mt-5 mb-8  flex flex-row text-base items-center ">
           <IoLocationSharp />
 
           <p className="font-thin ml-1  text-black-400">Event Location</p>
         </div>
-        <p className=" ml-1 mr-3 text-base md:text-sm">
+        <p className=" ml-1 mr-3 text-base">
           In publishing and graphic design, Lorem ipsum is a placeholder text
           commonly used to demonstrate the visual form of a document or a
           typeface without relying on meaningful content.
--- pages/Events.jsx
+++ pages/Events.jsx
@@ -19,39 +19,6 @@ function Events(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <PageTemplate>
-<<<<<<< HEAD
-      <div className='pb-24' >
-          
-          <div className='flex flex-col h-48 md:h-36 lg:h-48 justify-center 
-          ml-16 bg-red border-b-2 border-black'>
-              <div className='text-primary text-2xl font-bold py-7 md:text-3xl md:py-3 lg:text-4xl'>
-                  Events
-              </div>
-              <div className='flex flex-row items-center'>
-                  <p className='text-3xl md:text-xl lg:text-2xl '>categories</p>
-                  <select className='w-3/12 ml-4 p-2 rounded-full h-10 
-                  border border-black bg-white md:w-5/12 lg:w-2/12 lg:h-10 lg:px-1 '>
-                      <option>All</option>
-                      <option>All</option>
-                      <option>All</option>
-                      <option>All</option>
-
-                  </select>
-              </div>
-          </div>
-          <div className='w-fit flex flex-row justify-center '>
-
-          <div className='flex flex-row flex-wrap  ml-10 
-          w-12/12   md:ml-0'>
-            {
-                 EventData.map((val,i)=>{
-                     console.log(val)
-                     return <Event_Card index={i} />
-                 }) 
-              } 
-
-
-=======
         <div className="pb-24">
           <div className="flex flex-col h-48 justify-center ml-16 bg-red border-b-2 border-black">
             <div className="text-primary text-4xl font-bold py-7">Events</div>
@@ -72,7 +39,6 @@ function Events(props) {
                 return <Event_Card index={i} />;
               })}
             </div>
->>>>>>> c86362ade8c8bf1273c5a19dab2d8766949d4383
           </div>
         </div>
       </PageTemplate>
